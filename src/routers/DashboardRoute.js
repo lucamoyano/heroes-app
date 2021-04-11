07ddbf4c5a16d52d5d2dea810804400216032915
Lucas Moyano <lucas.95moyano@gmail.com>
2021-04-11 00:56:11 -0300

    Search hero

--- src/routers/DashboardRoute.js
+++ src/routers/DashboardRoute.js
@@ -3,6 +3,7 @@ import { Redirect, Route, Switch } from 'react-router';
 import { DcScreen } from '../components/dc/DcScreen';
 import { HeroeScreen } from '../components/heroes/HeroeScreen';
 import { MarvelScreen } from '../components/marvel/MarvelScreen';
+import { SearchScreen } from '../components/search/SearchScreen';
 
 import { Navbar } from '../components/ui/Navbar';
 
@@ -16,6 +17,7 @@ export const DashboardRoute = () => {
                     <Route exact path="/marvel" component={ MarvelScreen } />
                     <Route exact path="/hero/:heroeId" component={ HeroeScreen } />
                     <Route exact path="/dc" component={ DcScreen } />
+                    <Route exact path="/search" component={ SearchScreen } />
                     <Redirect to="/marvel" />
                 </Switch>
             </div>
